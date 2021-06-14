eab27d297362a5ec69ae0d1784ee2fe9fc7f5ff5
JosephAr1 <jonielhet@yahoo.com>
2021-06-14 19:18:34 +1200

    Add project files.

---
+++ SumofallOddnos_till100/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumofallOddnos_till100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, sum;
+            number1 = 2;
+            sum = 0;
+            while(number1 <=100)
+            {
+                Console.WriteLine(number1);
+                sum = sum + number1;
+                number1 = number1 + 2;
+            }
+            Console.WriteLine("Sum of all Primary numbers till 100 = " + sum.ToString());
+        }
+    }
+}
